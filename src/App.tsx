15a87592f46eee9eabc1ff8a5bab24c3abe9437b
deiker619 <deiker1842@gmail.com>
2025-08-08 18:33:32 -0400

    feat: se agregó el dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 
+import { Route, Routes } from "react-router-dom"
+import { MainLayout } from "./layouts/MainLayout"
+import { Home } from "./pages/dashboard/home"
+
+
+
 function App() {
-
-
   return (
     <>
-     
+
+      <Routes>
+        {/* Rutas con MainLayout */}
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home></Home>} />
+        </Route>
+
+       
+      </Routes>
+
     </>
   )
 }
