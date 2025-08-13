99eda9f18b2e5755c61382beda53675679ef0595
deiker619 <deiker1842@gmail.com>
2025-08-13 14:17:43 -0400

    fix: datos de la grafica

--- src/components/chart-area-interactive.tsx
+++ src/components/chart-area-interactive.tsx
@@ -168,10 +168,10 @@ export function ChartAreaInteractive() {
   return (
     <Card className="@container/card">
       <CardHeader>
-        <CardTitle>Total Visitors</CardTitle>
+        <CardTitle>Total Asistencias</CardTitle>
         <CardDescription>
           <span className="hidden @[540px]/card:block">
-            Total for the last 3 months
+            Total de los últimos 3 meses
           </span>
           <span className="@[540px]/card:hidden">Last 3 months</span>
         </CardDescription>
